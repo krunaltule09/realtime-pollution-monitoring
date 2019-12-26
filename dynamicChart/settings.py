6dc292373173357ed5dc832200dfbf4078aa7fe7
krunal <krunaltule53@gmail.com>
2019-12-27 00:57:28 +0530

    db added

--- dynamicChart/settings.py
+++ dynamicChart/settings.py
@@ -132,3 +132,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
